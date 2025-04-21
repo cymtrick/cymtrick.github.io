ba70b6ece81887b5352f9c2e432f07226eb68f85
cymtrick <32484790+cymtrick@users.noreply.github.com>
2025-04-21 04:20:48 +0000

    Deploying to gh-pages from @ cymtrick/cymtrick.github.io@0dbcc88066f57ed7e497c7c37140bf5c51297afa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -70,17 +70,6 @@ ninja.data = [{
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
-      },{id: "post-facebook-partners-csrf",
-        
-          title: "Facebook Partners Csrf",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/al-folio/blog/2016/Facebook-partners-CSRF/";
-          
-        },
       },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
